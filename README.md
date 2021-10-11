822108d5d1f01f9a401a6d3116e9238f087ce962
Calum Munro <cmunro@findmypast.com>
2021-10-11 21:03:27 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# paypal-billingAgreement-extractor
+
+We currently have an integration with Paypal, that integrated via the Braintree SDK. This is simply just a means of using Paypal, we are not using Braintree as a Payment Gateway. The Braintree SDK gives us a simple PaymentMethodToken, which translates to a Paypal BillingAgreementId. These AgreementId's are required for the import into our setup with Recurly. Braintree were slow to assist us in this, so we had to manually progress by using the API to gain these.
+
+This code represents a spike to determine the feasibility of the task.
+
+To Execute, simply run the following commands:
+
+Install dependencies
+`npm i`
+
+Run the Application
+`Node App.js`
---
+++ app.js
@@ -0,0 +1,31 @@
+const braintree = require("braintree");
+
+// intake a list of tokens, in csv or something else
+
+// running through them all, make the following search query to Braintree for the Paypal BillingAgreementId
+// If it exists, save it in the futurepay table in the new column PP_BilingAgreementId
+// Else, add to a csv of errored tokens
+
+// Establish a connection to Braintree Gateway
+var gateway = new braintree.BraintreeGateway({
+  //token value, stored in vault, production token found at the following location
+  accessToken: "'paypal/tokens/fmp/gbp/production'",
+});
+
+const paymentToken = "string_representing_token";
+
+const stream = gateway.customer.search(
+  (search) => {
+    search.paymentMethodToken().is(paymentToken);
+  },
+  (err, response) => {
+    response.each((err, customer) => {
+      // obv more error handling, this is a short spike to prove the theory.
+      console.log(customer.paypalAccounts[0].billingAgreementId);
+    });
+
+    if (err) {
+      console.log("error here");
+    }
+  }
+);
