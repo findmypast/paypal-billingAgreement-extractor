9538cd17d17dcaff39c0157b254bfebb06b8f696
Calum Munro <cmunro@findmypast.com>
2021-10-19 15:19:00 +0100

    THG-2120 :sparkles: Saves BillingInfoId to DB
    Co-authored-by: Aneesa Zafri <azafri@findmypast.com>

--- src/app.js
+++ src/app.js
@@ -2,17 +2,7 @@ const braintree = require("braintree");
 const fs = require("fs");
 const parse = require("csv-parse/lib/sync");
 const sql = require("mssql");
-
-// import { parse } from "csv-parse/sync";
-import { sqlConfig } from "./config";
-
-// Load a whole file into memory
-
-// do a search
-
-// process result
-
-//move onto the next one.
+const secrets = require("../secrets.json");
 
 const main = async (inputFilename) => {
   var inputFile = fs.readFileSync(`./${inputFilename}`);
@@ -25,56 +15,46 @@ const main = async (inputFilename) => {
 
   // Establish a connection to Braintree Gateway
   var gateway = new braintree.BraintreeGateway({
-    //token value, stored in vault, integration token found at the following location
-    accessToken: "paypal/tokens/fmp/gbp/integration",
+    //token value, stored in vault, integration token found at the following location uk
+    accessToken: secrets.uk_ie.integration,
   });
+
+  await sql.connect(secrets.sqlConfig);
+
   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
   for (const token of tokens) {
     if (requestCount % 10 === 0) {
-      await timer(1000);
+      await timer(150);
     }
 
-    const customerStream = gateway.customer.search(
-      (search) => {
+    gateway.customer.search(
+      async (search) => {
         // Examples of actual payment tokens can be found in the futurepay table.
         search.paymentMethodToken().is(token[0]);
       },
       (err, response) => {
-        response.first((err, customer) => {
-          //console.log(err);
+        response.first(async (err, customer) => {
           if (customer) {
             process.stdout.write(
               `${outputCount++} ${token[0]} ${
                 customer.paypalAccounts[0].billingAgreementId
               }\n`
             );
+            // try {
+            //   const result = await sql.query(`UPDATE future_pay
+            //       SET paypal_billing_agreement_id = '${customer.paypalAccounts[0].billingAgreementId}'
+            //       WHERE [future_pay_id] = '${token[0]}';`);
+            // } catch (err) {
+            //   // ... error checks
+            // }
           } else {
-            console.log(`${outputCount++} ${token[0]}`);
+            process.stdout.write(`${outputCount++} ${token[0]}\n`);
             failedCount++;
           }
         });
       }
     );
-
-    // customerStream.on("readable", () => {
-    //   const customer = customerStream.read();
-
-    //   if (customer) {
-    //     process.stdout.write(
-    //       `${outputCount++} ${token[0]} ${
-    //         customer.paypalAccounts[0].billingAgreementId
-    //       }\n`
-    //     );
-    //   } else {
-    //     console.log(`failed: ${failedCount++}`);
-    //   }
-
-    //   // process.stdout.write(
-    //   //         `${outputCount++} ${token[0]} ${
-    //   //           customer.paypalAccounts[0].billingAgreementId
-    //   // )
-    // });
 
     requestCount++;
   }
@@ -93,17 +73,3 @@ const main = async (inputFilename) => {
 
   process.stdout.write("...done\n");
 })();
-
-//  Populate db
-// async () => {
-//   try {
-//     await sql.connect(sqlConfig);
-//     const result =
-//       await sql.query`INSERT INTO [HappyGeneral_integration].[dbo].[future_pay](paypal_billing_agreement_id)
-//     VALUES ('')
-//     WHERE future_pay_id = 'asdqwe'`;
-//     console.dir(result);
-//   } catch (err) {
-//     // ... error checks
-//   }
-// };
